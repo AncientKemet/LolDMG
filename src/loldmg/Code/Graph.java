ea82db000d80b61ca058873f17afbd077de8f3b3
unknown <gaspar093@hotmail.com>
2014-11-11 21:51:34 +0200

    recursive depth first search of all possible item builds withing gold range no item upgrading atm

--- src/loldmg/Code/Graph.java
+++ src/loldmg/Code/Graph.java
@@ -7,6 +7,8 @@ package loldmg.Code;
 
 import dto.Static.Champion;
 import dto.Static.ChampionSpell;
+import dto.Static.Item;
+import java.util.ArrayList;
 import java.util.List;
 import loldmg.Code.Interfaces.GraphInterface;
 import static loldmg.Code.dmg_calculation.dmgafterdefence;
@@ -15,60 +17,58 @@ import static loldmg.Code.dmg_calculation.dmgafterdefence;
  *
  * @author Robert
  */
-public class Graph  {
+public class Graph {
 
-    boolean debug = true;
+   static boolean debug = true;
 
-    public void Update() {
-        for (int level = 0; level < 18; level++) {
+
+    public static int rotationdmg(Champion attacker, Champion target, List<ChampionSpell> rotation,ArrayList<Item> purchasedItems, int level) {
+        double damage = 0;
+        
+        if (rotation != null) {
+            if (debug) {
+                System.out.println(attacker.getName() + " vs " + target.getName());
+            }
+
+            damage = 0;
+            for (int k = 0; k < rotation.size(); k++) {
+
+                if (rotation.get(k).getVars() == null) {
+                    damage += dmgafterdefence(attacker.getStats().getAttackdamage() + attacker.getStats().getAttackdamageperlevel() * level, target.getStats().getArmor() + target.getStats().getArmorperlevel() * level, 0, 0);
+                }
+            }
+            if (debug) {
+                System.out.println("dmg = " + damage);
+            }
+            return (int) (Math.round(damage));
 
         }
+        return (0);
     }
 
-    public void rotationdmg(Champion attacker, Champion target, List<ChampionSpell> rotation,GraphInterface graphInterface) {
+    public static int rotationdps(Champion attacker, Champion target, List<ChampionSpell> rotation,ArrayList<Item> purchasedItems, int level) {
         double damage = 0;
         if (rotation != null) {
             if (debug) {
                 System.out.println(attacker.getName() + " vs " + target.getName());
             }
 
-            for (int i = 0; i < 18; i++) {
-                damage=0;
-                for (int k = 0; k < rotation.size(); k++) {
-                    
-                    if (rotation.get(k).getVars() == null) {
-                        damage += dmgafterdefence(attacker.getStats().getAttackdamage() + attacker.getStats().getAttackdamageperlevel() * i, target.getStats().getArmor() + target.getStats().getArmorperlevel() * i, 0, 0);
-                    }
+            damage = 0;
+
+            for (int k = 0; k < rotation.size(); k++) {
+
+                if (rotation.get(k).getVars() == null) {
+                    damage += dmgafterdefence(attacker.getStats().getAttackdamage() + attacker.getStats().getAttackdamageperlevel() * level, target.getStats().getArmor() + target.getStats().getArmorperlevel() * level, 0, 0) * (0.625 / (1 - attacker.getStats().getAttackspeedoffset())) * (1 + (attacker.getStats().getAttackspeedperlevel() * level / 100));
                 }
-                graphInterface.NewData((int) Math.round(damage),i);
-                if (debug) {
-                            System.out.println("dmg = " + damage);
-                        }
             }
-        }
-    }
-        public void rotationdps(Champion attacker, Champion target, List<ChampionSpell> rotation,GraphInterface graphInterface) {
-        double damage = 0;
-        if (rotation != null) {
             if (debug) {
-                System.out.println(attacker.getName() + " vs " + target.getName());
+                System.out.println("dmg = " + damage);
             }
+            return (int) (Math.round(damage));
 
-            for (int level = 0; level < 18; level++) {
-                damage=0;
-
-                for (int k = 0; k < rotation.size(); k++) {
-                    
-                    if (rotation.get(k).getVars() == null) {
-                        damage += dmgafterdefence(attacker.getStats().getAttackdamage() + attacker.getStats().getAttackdamageperlevel() * level, target.getStats().getArmor() + target.getStats().getArmorperlevel() * level, 0, 0)*(0.625 / (1 - attacker.getStats().getAttackspeedoffset()))*(1+(attacker.getStats().getAttackspeedperlevel() * level / 100));
-                    }
-                }
-                graphInterface.NewData((int) Math.round(damage),level);
-                if (debug) {
-                            System.out.println("dmg = " + damage);
-                        }
-            }
         }
+        return(0);
+
     }
 
 }
--- src/loldmg/Code/ItemUtils.java
+++ src/loldmg/Code/ItemUtils.java
@@ -6,34 +6,50 @@
 package loldmg.Code;
 
 import dto.Static.Champion;
+import dto.Static.ChampionSpell;
 import dto.Static.Item;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Robert
  */
 public class ItemUtils {
-    
+
     /**
-     * this should do:
-     * purchase/complete new item
-     * if u complete new item u gotta remove the original items.
-    */
-    public static Item PurchaseItem(ArrayList<Item> purchasedItems, Item itemToPurchase)
-    {
-        
-    }
-    
-    /**
-     * this should do:
-     * do compare dps for each item in loldmg.LolDmg.itemList
-     * and find the one that increases dps by most
-     * also keep in mind that u need to have at max 6x items in purchasedItems
-     * 
+     * this should do: purchase/complete new item if u complete new item u gotta
+     * remove the original items.
      */
-    public static Item FindBestItemToPurchase(ArrayList<Item> purchasedItems, Champion myChamp, Champion target, int level, int currentGold)
-    {
+    static ArrayList<Item> finalItems;
+    static int dps=0;
+    static int gold=0;
+
+    public static void PurchaseItem(ArrayList<Item> purchasedItems, ArrayList<Item> purchasableItems,Champion myChamp, Champion target, int level, int cash) {
+        if (purchasedItems.size() < 6) {
+            for (int i = 0; i < purchasableItems.size(); i++) {
+                if (purchasableItems.get(i).getGold().getBase() < cash) {
+                    purchasedItems.add(purchasableItems.get(i));
+                    PurchaseItem(purchasedItems, purchasableItems,myChamp,target,level, cash - purchasableItems.get(i).getGold().getBase());
+                }
+            }
+        }
+        System.out.println("dps goes here");
+
+        //calculate dps for each item
+        List<ChampionSpell> rotation = null;
+        ChampionSpell aa=new ChampionSpell();
+        rotation.add(aa);
+        int dps2=Graph.rotationdps(myChamp,target,rotation,purchasedItems,level);
+        if(dps2>dps){
+            dps=dps2;
+            finalItems=purchasedItems;
+            gold=cash;}
     }
-    
+
+    public static ArrayList<Item> FindBestItemToPurchase(ArrayList<Item> currentItems, ArrayList<Item> purchasableItems,Champion myChamp, Champion target, int level, int currentGold) {
+        PurchaseItem(currentItems, purchasableItems,myChamp,target,level,currentGold);
+        return (finalItems);
+    }
+
 }
